d1afddfd1b2a087b4d6cf5c8590e9d47636a9c82
Seungchul Kim <sc377.kim@samsung.com>
2019-11-04 13:59:52 +0900

    libhwc2.1: fix bug of setVsyncEnabled for external display
    Bug: 164139096
    Change-Id: I2964ba60bacb2f9641eb9f5cc17d9476816a6551
    Signed-off-by: Seungchul Kim <sc377.kim@samsung.com>

--- base/libhwc2.1/libexternaldisplay/ExynosExternalDisplay.h
+++ base/libhwc2.1/libexternaldisplay/ExynosExternalDisplay.h
@@ -51,7 +51,6 @@ class ExynosExternalDisplay : public ExynosDisplay {
         virtual int openExternalDisplay();
         virtual void closeExternalDisplay();
         virtual int32_t getActiveConfig(hwc2_config_t* outconfig);
-        virtual int32_t setVsyncEnabled(int32_t /*hwc2_vsync_t*/ enabled);
         virtual int32_t startPostProcessing();
         virtual int32_t setClientTarget(
                 buffer_handle_t target,
