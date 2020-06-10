314bcb9ec3a19151c3a3b33803dee9568b60c7ab
HyunKyung Kim <hk310.kim@samsung.com>
2020-06-10 16:03:30 +0900

    libhwc2.1: Check nullptr parameter of HWC API
    Signed-off-by: HyunKyung Kim <hk310.kim@samsung.com>
    Change-Id: Ib042aaf4b799580a86a9c7d21781b4ce00237290

--- base/libhwc2.1/ExynosHWC.cpp
+++ base/libhwc2.1/ExynosHWC.cpp
@@ -229,6 +229,9 @@ int32_t exynos_createLayer(hwc2_device_t *dev,
 int32_t exynos_createVirtualDisplay(hwc2_device_t *dev, uint32_t width, uint32_t height,
         int32_t *format, hwc2_display_t *outDisplay)
 {
+    if (format == nullptr)
+        return HWC2_ERROR_BAD_PARAMETER;
+
     ExynosDevice *exynosDevice = checkDevice(dev);
     *outDisplay = getDisplayId(HWC_DISPLAY_VIRTUAL, 0);
 
@@ -618,6 +621,9 @@ int32_t exynos_setColorMode(hwc2_device_t *dev, hwc2_display_t display, int32_t 
 int32_t exynos_setColorTransform(hwc2_device_t *dev, hwc2_display_t display,
         const float* matrix, int32_t hint)
 {
+    if (matrix == nullptr)
+        return HWC2_ERROR_BAD_PARAMETER;
+
     ExynosDevice *exynosDevice = checkDevice(dev);
 
     if (exynosDevice) {
@@ -990,6 +996,9 @@ int32_t exynos_setLayerPerFrameMetadata(hwc2_device_t *dev, hwc2_display_t displ
         hwc2_layer_t layer, uint32_t numElements,
         const int32_t* /*hw2_per_frame_metadata_key_t*/ keys,
         const float* metadata) {
+    if ((keys == nullptr) || (metadata == nullptr))
+        return HWC2_ERROR_BAD_PARAMETER;
+
     ExynosDevice *exynosDevice = checkDevice(dev);
     if (exynosDevice) {
         ExynosDisplay *exynosDisplay = checkDisplay(exynosDevice, display);
@@ -1111,6 +1120,9 @@ int32_t exynos_setLayerPerFrameMetadataBlobs(hwc2_device_t* dev, hwc2_display_t 
         hwc2_layer_t layer, uint32_t numElements, const int32_t* keys, const uint32_t* sizes,
         const uint8_t* metadata)
 {
+    if ((keys == nullptr) || (sizes == nullptr) || (metadata == nullptr))
+        return HWC2_ERROR_BAD_PARAMETER;
+
     ExynosDevice *exynosDevice = checkDevice(dev);
 
     if (exynosDevice) {
